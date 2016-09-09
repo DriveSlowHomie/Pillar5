eb0db6e0e374245a19227362a3dde56a741a859b
Linda Zou <linda.ye.zou@gmail.com>
2016-09-08 17:35:48 -0700

    Adds logic to save post to existing user

--- routes/postRoute.ts
+++ routes/postRoute.ts
@@ -2,6 +2,8 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 let Post = require('../models/postModel')
+let User = require('../models/userModel')
+
 
 let post = new Post();
 
@@ -10,10 +12,31 @@ router.post('/fileUpload', function(req, res, next) {
     post.description = req.body.description;
     post.pillar = req.body.pillar;
     post.region = req.body.region;
-    post.save(function(err, post) {
-       if(err) return next(err);
-       res.send("File Uploaded!");
-    });
+//     post.save(function(err, post) {
+//        if(err) return next(err);
+//        res.send("File Uploaded!");
+//     });
+
+    User.findOne({email: "123"}, (err, user) => {
+      if(err) {
+        res.send(err);
+      }
+      if(user) {
+        let postArray = user.posts;
+        postArray.push(post);
+        user.posts = postArray;
+        user.save((err, user) => {
+          if(err) {
+            res.send(err)
+          }
+        })
+      }
+    })
+
+    post.save(function(err, location) {
+         if(err) return next(err);
+         res.send("This post has been added");
+    });
 });
 
 export = router;
