18b74a7815501b62da0ea0ead4d43b456943b30b
Linda Zou <linda.ye.zou@gmail.com>
2016-09-20 11:09:01 -0700

    Adds simple layout to userprofile

--- ngApp/authInterceptor.ts
+++ ngApp/authInterceptor.ts
@@ -1,13 +1,33 @@
+// namespace app {
+//   angular.module('app').factory('AuthInterceptor', function($window){
+//     return {
+//       request: function(config) {
+//         config.header = config.header || {};
+//         if($window.localStorage.getItem('token')){
+//           config.headers.Authorization = 'Bearer' + $window.localStorage.getItem('token')
+//         }
+//         return config;
+//       }
+//     }
+//   })
+// }
+
+
 namespace app {
-  angular.module('app').factory('AuthInterceptor', function($window){
+  angular.module('app').factory('AuthInterceptor', function($window, $state) {
     return {
       request: function(config) {
-        config.header = config.header || {};
-        if($window.localStorage.getItem('token')){
-          config.headers.Authorization = 'Bearer' + $window.localStorage.getItem('token')
+        console.log("AuthInterceptor IS BEING CALLED++++++++++++++++++++++", config)
+        config.headers = config.headers || { };
+        if ($window.localStorage.getItem('token')) {
+          console.log("Token from AuthInterceptor ", $window.localStorage.getItem('token'))
+          config.headers.Authorization = 'Bearer ' + $window.localStorage.getItem('token');
+        } else {
+          this.$state.go("Home");
+          // this.$state.go('UserProfile')
         }
         return config;
       }
     }
-  })
+  });
 }
